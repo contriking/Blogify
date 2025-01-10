834f049e91160f647b77e2c02196240e31dc8f8e
Prashant Sahu <prashantsahups100@gmail.com>
2025-01-11 01:30:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app=express();
 // app.use(express.json({extended : true}))
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-app.use(cors({credentials : true , origin: "http://localhost:5173"}))
+app.use(cors({credentials : true , origin: "https://blogify-frontend-mfkq.onrender.com"}))
 
 app.use(upload())  
 app.use('/uploads',express.static(__dirname + '/uploads'))
